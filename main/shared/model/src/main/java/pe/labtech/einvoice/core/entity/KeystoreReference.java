e581ad272d18f04b1859350c1384601b5a350b16
Carlos Echeverría <egcarlos@gmail.com>
2015-03-13 14:57:26 -0500

    Entidades para almacenar los keystores

---
+++ main/shared/model/src/main/java/pe/labtech/einvoice/core/entity/KeystoreReference.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.labtech.einvoice.core.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Carlos
+ */
+@Entity
+@Table(name = "CF_KEYSTORE")
+public class KeystoreReference implements Serializable {
+
+    @Id
+    @Column(name = "ID", length = 40)
+    private String id;
+
+    @Lob
+    @Column(name = "KEYSTORE_DATA")
+    private byte[] data;
+
+    @Column(name = "PROTECTION")
+    private String protection;
+
+    public KeystoreReference() {
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 73 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final KeystoreReference other = (KeystoreReference) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    public String getProtection() {
+        return protection;
+    }
+
+    public void setProtection(String protection) {
+        this.protection = protection;
+    }
+
+}
---
+++ main/shared/model/src/main/java/pe/labtech/einvoice/core/entity/SecurityValues.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.labtech.einvoice.core.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Carlos
+ */
+@Entity
+@Table(name = "CF_SECURITYVALUES")
+public class SecurityValues implements Serializable {
+
+    @Id
+    @Column(length = 40)
+    private String userId;
+
+    @JoinColumn(name = "KEYSTORE_ID")
+    @ManyToOne
+    private KeystoreReference keystoreReference;
+
+    @Column(name = "ALIAS", length = 100)
+    private String alias;
+    @Column(name = "PROTECTION", length = 100)
+    private String protection;
+
+    public SecurityValues() {
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.userId);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final SecurityValues other = (SecurityValues) obj;
+        if (!Objects.equals(this.userId, other.userId)) {
+            return false;
+        }
+        return true;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public KeystoreReference getKeystoreReference() {
+        return keystoreReference;
+    }
+
+    public void setKeystoreReference(KeystoreReference keystoreReference) {
+        this.keystoreReference = keystoreReference;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getProtection() {
+        return protection;
+    }
+
+    public void setProtection(String protection) {
+        this.protection = protection;
+    }
+
+}
