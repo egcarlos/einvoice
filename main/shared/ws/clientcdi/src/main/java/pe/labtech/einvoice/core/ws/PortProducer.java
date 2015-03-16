dab1b6aa839090c38f69c0d9a7a614b0805ee325
Carlos Echeverría <egcarlos@gmail.com>
2015-03-15 22:05:32 -0500

    wsclient 1.0.1 snapshot
    SNAPSHOT build que agrega al flujo el programa de pruebas y el campo
    para el URL del cdr.
    Se reordenan las carpetas.
    Se genera un parent al grupo de servicios web.

---
+++ main/shared/ws/clientcdi/src/main/java/pe/labtech/einvoice/core/ws/PortProducer.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.labtech.einvoice.core.ws;
+
+import java.util.Map;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.xml.ws.BindingProvider;
+import pe.labtech.einvoice.core.ws.generated.EBizGenericInvoker;
+import pe.labtech.einvoice.core.ws.generated.EBizGenericInvokerImplService;
+
+/**
+ *
+ * @author Carlos
+ */
+@Dependent
+public class PortProducer {
+
+    private static final Logger logger = Logger.getLogger("PortProducer");
+
+    private final EBizGenericInvokerImplService service;
+
+    private final PortInfo info;
+
+    @Inject
+    public PortProducer(EBizGenericInvokerImplService service, PortInfo info) {
+        this.service = service;
+        this.info = info;
+    }
+
+    ThreadLocal<EBizGenericInvoker> localPort;
+
+    @PostConstruct
+    public void init() {
+        localPort = new ThreadLocal<EBizGenericInvoker>() {
+            @Override
+            protected EBizGenericInvoker initialValue() {
+                EBizGenericInvoker port = service.getEBizGenericInvokerImplPort();
+//                Map<String, Object> rc = ((BindingProvider) port).getRequestContext();
+                logger.info("port built for thread");
+                return port;
+            }
+        };
+
+    }
+
+    @Produces
+    public EBizGenericInvoker getPort() {
+        final EBizGenericInvoker port = localPort.get();
+        Map<String, Object> rc = ((BindingProvider) port).getRequestContext();
+        rc.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, info.getEndpoint());
+        rc.put(BindingProvider.USERNAME_PROPERTY, info.getUser());
+        rc.put("javax.xml.ws.client.connectionTimeout", info.getConnectionTimeout());
+        rc.put("javax.xml.ws.client.receiveTimeout", info.getReceiveTimeout());
+        rc.put(BindingProvider.PASSWORD_PROPERTY, info.getPassword());
+        return port;
+    }
+
+}
