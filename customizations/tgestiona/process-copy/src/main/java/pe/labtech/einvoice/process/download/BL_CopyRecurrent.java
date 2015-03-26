39cd77db9da6e695ba19e7dc1a3bccd4fa0775a0
Carlos Echeverría <egcarlos@gmail.com>
2015-03-26 04:27:35 -0500

    BIGFIX reparado proceso copia tgestiona
    se ha reparado y refactorizado totalmente el proceso elaborado por
    bizlinks para la copia de archivos en modelo tgestiona

---
+++ customizations/tgestiona/process-copy/src/main/java/pe/labtech/einvoice/process/download/BL_CopyRecurrent.java
@@ -0,0 +1,149 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.labtech.einvoice.process.download;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.stream.Collectors;
+import javax.annotation.PostConstruct;
+import javax.ejb.ConcurrencyManagement;
+import javax.ejb.ConcurrencyManagementType;
+import javax.ejb.EJB;
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import pe.labtech.einvoice.commons.recurrent.AbstractRecurrentTask;
+import pe.labtech.einvoice.core.entity.Document;
+import pe.labtech.einvoice.core.entity.DocumentData;
+import pe.labtech.einvoice.core.model.DocumentDataLoaderLocal;
+import static pe.labtech.einvoice.core.model.DocumentDataLoaderLocal.*;
+import pe.labtech.einvoice.replicator.entity.CancelHeader;
+import pe.labtech.einvoice.replicator.entity.CancelHeaderPK;
+import pe.labtech.einvoice.replicator.entity.DocumentHeader;
+import pe.labtech.einvoice.replicator.entity.DocumentHeaderPK;
+import pe.labtech.einvoice.replicator.entity.SummaryHeader;
+import pe.labtech.einvoice.replicator.entity.SummaryHeaderPK;
+import pe.labtech.einvoice.replicator.model.PublicDatabaseManagerLocal;
+
+/**
+ *
+ * @author Carlos
+ */
+@Singleton
+@ConcurrencyManagement(ConcurrencyManagementType.BEAN)
+@TransactionManagement(TransactionManagementType.BEAN)
+public class BL_CopyRecurrent extends AbstractRecurrentTask<DocumentData> {
+
+    @EJB
+    DocumentDataLoaderLocal loader;
+
+    @EJB
+    PublicDatabaseManagerLocal db;
+
+    public BL_CopyRecurrent() {
+    }
+
+    @EJB
+    PublicDatabaseManagerLocal manager;
+
+    private Map<Long, Document> lista;
+
+    @Override
+    @PostConstruct
+    public void init() {
+        lista = new HashMap<>();
+        super.init();
+        this.findTasks = () -> loader.find(DATA_LOADED);
+        this.tryLock = t -> loader.changeStatus(t, DATA_LOADED, DATA_COPYING);
+        this.getId = t -> t.getDocument().getClientId() + "-" + t.getDocument().getDocumentType() + "-" + t.getDocument().getDocumentNumber() + "[copy:" + t.getName() + "]";
+        this.consumer = t -> {
+            try {
+                lista.put(t.getDocument().getId(), t.getDocument());
+            } catch (Exception ex) {
+                logger.log(Level.SEVERE, null, ex);
+            }
+        };
+    }
+
+    //TODO ajustar el tiempo de llamada luego de pruebas
+    @Override
+    @Schedule(hour = "17", minute = "32", second = "00", persistent = false)
+    protected void timeout() {
+        super.timeout();
+        try (BL_HandleFile hf = new BL_HandleFile()) {
+            logger.log(Level.WARNING, () -> "tamanio de lsta.............." + lista.size());
+            List listDocumentHeader = lista.values().stream().map(d -> getEinvoiceHeader(d)).filter(d -> d != null).collect(Collectors.toList());
+            if (listDocumentHeader.isEmpty()) {
+                return;
+            }
+            hf.setListDocument(listDocumentHeader);
+            hf.connectToSftp();
+            hf.generateFileAndSendFilesToSftp();
+            logger.log(Level.WARNING, () -> "tamanio de listDocumentHeader.............." + listDocumentHeader.size());
+        } catch (Exception ex) {
+            logger.log(Level.SEVERE, ex, () -> "error raised while sending via sftp. " + ex);
+        } finally {
+            lista.clear();
+        }
+    }
+
+    private Object getEinvoiceHeader(Document t) {
+        String targetEntity;
+        Object id;
+
+        if (isInvoice(t)) {
+            id = new DocumentHeaderPK(
+                    t.getClientId().split("-")[0],
+                    t.getClientId().split("-")[1],
+                    t.getDocumentType(),
+                    t.getDocumentNumber()
+            );
+            targetEntity = "DocumentHeader";
+        } else if (isInvoiceSummary(t)) {
+            id = new SummaryHeaderPK(
+                    t.getClientId().split("-")[0],
+                    t.getClientId().split("-")[1],
+                    t.getDocumentNumber()
+            );
+            targetEntity = "SummaryHeader";
+        } else if (isCancelSummary(t)) {
+            id = new CancelHeaderPK(
+                    t.getClientId().split("-")[0],
+                    t.getClientId().split("-")[1],
+                    t.getDocumentNumber()
+            );
+            targetEntity = "CancelHeader";
+        } else {
+            //invalid forged data
+            return null;
+        }
+        switch (targetEntity) {
+            case "DocumentHeader":
+                return db.seek(e -> e.find(DocumentHeader.class, id));
+            case "SummaryHeader":
+                return db.seek(e -> e.find(SummaryHeader.class, id));
+            case "CancelHeader":
+                return db.seek(e -> e.find(CancelHeader.class, id));
+        }
+        return null;
+    }
+
+    private static boolean isCancelSummary(Document t) {
+        return t.getDocumentNumber().startsWith("RA");
+    }
+
+    private static boolean isInvoiceSummary(Document t) {
+        return t.getDocumentNumber().startsWith("RC");
+    }
+
+    private static boolean isInvoice(Document t) {
+        return t.getDocumentNumber().startsWith("F") | t.getDocumentNumber().startsWith("B");
+    }
+}
---
+++ customizations/tgestiona/process-copy/src/main/java/pe/labtech/einvoice/process/download/BL_HandleFile.java
@@ -0,0 +1,353 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.labtech.einvoice.process.download;
+
+import com.jcraft.jsch.*;
+import java.io.BufferedWriter;
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Properties;
+import pe.labtech.einvoice.replicator.entity.CancelHeader;
+import pe.labtech.einvoice.replicator.entity.DocumentHeader;
+import pe.labtech.einvoice.replicator.entity.SummaryHeader;
+
+/**
+ *
+ * @author martin
+ */
+public class BL_HandleFile implements Closeable {
+
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(BL_HandleFile.class.getName());
+    private JSch jsch;
+    private Session session;
+    private ChannelSftp sftp;
+    private List<Object> listDocument;
+
+    //FIXME se debe serializar al disco las credenciales
+    public void connectToSftp() {
+        try {
+            //obtener las credenciales del disco
+            Properties p = new Properties();
+            p.load(new FileReader("tgestiona.properties"));
+            String user = p.getProperty("user");
+            String password = p.getProperty("password");
+            String server = p.getProperty("server");
+            int port = Integer.parseInt(p.getProperty("port"));
+
+            jsch = new JSch();
+            //session = jsch.getSession("sftpdesarrollo", "10.4.50.226", 22);
+            session = jsch.getSession(user, server, port);
+            Properties prop = new Properties();
+            prop.put("StrictHostKeyChecking", "no");
+            session.setConfig(prop);
+            //session.setPassword("tgestiona");
+            session.setPassword(password);
+            session.connect();
+            sftp = (ChannelSftp) session.openChannel("sftp");
+            sftp.connect();
+        } catch (IOException | NumberFormatException | JSchException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void generateFileAndSendFilesToSftp() {
+        Calendar c = Calendar.getInstance();
+        String dd = c.get(Calendar.DATE) + "";
+        String mm = c.get(Calendar.MONTH + 1) + "";
+        String yy = c.get(Calendar.YEAR) + "";
+
+        FileWriter fichero = null;
+        BufferedWriter bw = null;
+        File f = new File("../downloads/" + dd + mm + yy + ".txt");
+
+        try {
+            fichero = new FileWriter(f.getAbsolutePath());
+            bw = new BufferedWriter(fichero);
+
+            for (Object o : listDocument) {
+                if (o instanceof DocumentHeader) {
+                    sendFilesToSftp((DocumentHeader) o);
+                    generateFile((DocumentHeader) o, fichero, bw);
+                } else if (o instanceof SummaryHeader) {
+                    sendFilesToSftp((SummaryHeader) o);
+                    generateFile((SummaryHeader) o, fichero, bw);
+                } else if (o instanceof CancelHeader) {
+                    sendFilesToSftp((CancelHeader) o);
+                    generateFile((CancelHeader) o, fichero, bw);
+                }
+
+            }
+            bw.close();
+            sftp.put(new FileInputStream(f), f.getName());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fichero != null) {
+                    fichero.close();
+                }
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private void sendFilesToSftp(SummaryHeader dh) {
+
+        byte[] xmlUbl = dh.getBl_xmlubl();
+        byte[] xmlPdf = dh.getBl_pdf();
+        byte[] xmlCdr = dh.getBl_cdr();
+        byte[] xmlBizlink = dh.getBl_xml();
+        try {
+            String fileName = " ";
+
+            if (xmlUbl != null) {
+                fileName = "../downloads/" + dh.getId().getNumeroDocumentoEmisor() + "-" + dh.getId().getTipoDocumentoEmisor() + "-" + dh.getFechaEmisionComprobante() + "-" + dh.getId().getResumenId() + ".xml";
+                buildFile(xmlUbl, fileName);
+            }
+
+            if (xmlCdr != null) {
+                fileName = "../downloads/R-" + dh.getId().getNumeroDocumentoEmisor() + "-" + dh.getId().getTipoDocumentoEmisor() + "-" + dh.getFechaEmisionComprobante() + "-" + dh.getId().getResumenId() + ".xml";
+                buildFile(xmlCdr, fileName);
+            }
+
+            if (xmlBizlink != null) {
+                fileName = "../downloads/" + dh.getId().getNumeroDocumentoEmisor() + "-" + dh.getId().getTipoDocumentoEmisor() + "-" + dh.getFechaEmisionComprobante() + "-" + dh.getId().getResumenId() + "-B.xml";
+                buildFile(xmlBizlink, fileName);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void generateFile(SummaryHeader dh, FileWriter fichero, BufferedWriter bw) throws IOException {
+
+        StringBuilder s = new StringBuilder("");
+
+        if (dh.getBl_xml() != null) {
+            s.append(dh.getId().getNumeroDocumentoEmisor()).append("-").append(dh.getId().getTipoDocumentoEmisor()).append("-").append(dh.getFechaEmisionComprobante()).append("-").append(dh.getId().getResumenId()).append("-B.xml");
+        } else {
+            s.append(" ");
+        }
+
+        s.append("|");
+
+        if (dh.getBl_xmlubl() != null) {
+            s.append(dh.getId().getNumeroDocumentoEmisor()).append("-").append(dh.getId().getTipoDocumentoEmisor()).append("-").append(dh.getFechaEmisionComprobante()).append("-").append(dh.getId().getResumenId()).append(".xml");
+        } else {
+            s.append(" ");
+        }
+
+        s.append("|");
+
+        if (dh.getBl_cdr() != null) {
+            s.append("R-").append(dh.getId().getNumeroDocumentoEmisor()).append("-").append(dh.getId().getTipoDocumentoEmisor()).append("-").append(dh.getFechaEmisionComprobante()).append("-").append(dh.getId().getResumenId()).append(".xml");
+        } else {
+            s.append(" ");
+        }
+
+        s.append("|");
+
+        s.append(" ");
+
+        s.append("|");
+
+        //s.append(dh.getCodigoAuxiliar40_3()).append("|").append(dh.getFechaEmisionComprobante());
+        s.append(" ").append("|").append(dh.getFechaEmisionComprobante());
+
+        bw.write(s.append("\n").toString());
+    }
+
+    private void sendFilesToSftp(CancelHeader dh) {
+
+        byte[] xmlUbl = dh.getBl_xmlubl();
+        byte[] xmlPdf = dh.getBl_pdf();
+        byte[] xmlCdr = dh.getBl_cdr();
+        byte[] xmlBizlink = dh.getBl_xml();
+        try {
+            String fileName = " ";
+
+            if (xmlUbl != null) {
+                fileName = "../downloads/" + dh.getId().getNumeroDocumentoEmisor() + "-" + dh.getResumenTipo() + "-" + dh.getFechaEmisionComprobante() + "-" + dh.getId().getResumenId() + ".xml";
+                buildFile(xmlUbl, fileName);
+            }
+
+            if (xmlCdr != null) {
+                fileName = "../downloads/R-" + dh.getId().getNumeroDocumentoEmisor() + "-" + dh.getResumenTipo() + "-" + dh.getFechaEmisionComprobante() + "-" + dh.getId().getResumenId() + ".xml";
+                buildFile(xmlCdr, fileName);
+            }
+
+            if (xmlBizlink != null) {
+                fileName = "../downloads/" + dh.getId().getNumeroDocumentoEmisor() + "-" + dh.getResumenTipo() + "-" + dh.getFechaEmisionComprobante() + "-" + dh.getId().getResumenId() + "-B.xml";
+                buildFile(xmlBizlink, fileName);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void generateFile(CancelHeader dh, FileWriter fichero, BufferedWriter bw) throws IOException {
+
+        StringBuilder s = new StringBuilder("");
+
+        if (dh.getBl_xml() != null) {
+            s.append(dh.getId().getNumeroDocumentoEmisor()).append("-").append(dh.getResumenTipo()).append("-").append(dh.getFechaEmisionComprobante()).append("-").append(dh.getId().getResumenId()).append("-B.xml");
+        } else {
+            s.append(" ");
+        }
+
+        s.append("|");
+
+        if (dh.getBl_xmlubl() != null) {
+            s.append(dh.getId().getNumeroDocumentoEmisor()).append("-").append(dh.getResumenTipo()).append("-").append(dh.getFechaEmisionComprobante()).append("-").append(dh.getId().getResumenId()).append(".xml");
+        } else {
+            s.append(" ");
+        }
+
+        s.append("|");
+
+        if (dh.getBl_cdr() != null) {
+            s.append("R-").append(dh.getId().getNumeroDocumentoEmisor()).append("-").append(dh.getResumenTipo()).append("-").append(dh.getFechaEmisionComprobante()).append("-").append(dh.getId().getResumenId()).append(".xml");
+        } else {
+            s.append(" ");
+        }
+
+        s.append("|");
+
+        s.append(" ");
+
+        s.append("|");
+
+        //s.append(dh.getCodigoAuxiliar40_3()).append("|").append(dh.getFechaEmisionComprobante());
+        s.append(" ").append("|").append(dh.getFechaEmisionComprobante());
+
+        bw.write(s.append("\n").toString());
+    }
+
+    private void sendFilesToSftp(DocumentHeader dh) {
+
+        byte[] xmlUbl = dh.getBl_xmlubl();
+        byte[] xmlPdf = dh.getBl_pdf();
+        byte[] xmlCdr = dh.getBl_cdr();
+        byte[] xmlBizlink = dh.getBl_xml();
+        try {
+            String fileName = " ";
+
+            if (xmlUbl != null) {
+                fileName = "../downloads/" + dh.getId().getNumeroDocumentoEmisor() + "-" + dh.getId().getTipoDocumento() + "-" + dh.getId().getSerieNumero() + ".xml";
+                buildFile(xmlUbl, fileName);
+            }
+
+            if (xmlPdf != null) {
+                fileName = "../downloads/" + dh.getId().getNumeroDocumentoEmisor() + "-" + dh.getId().getTipoDocumento() + "-" + dh.getId().getSerieNumero() + ".pdf";
+                buildFile(xmlPdf, fileName);
+            }
+
+            if (xmlCdr != null) {
+                fileName = "../downloads/R" + dh.getId().getNumeroDocumentoEmisor() + "-" + dh.getId().getTipoDocumento() + "-" + dh.getId().getSerieNumero() + ".xml";
+                buildFile(xmlCdr, fileName);
+            }
+
+            if (xmlBizlink != null) {
+                fileName = "../downloads/" + dh.getId().getNumeroDocumentoEmisor() + "-" + dh.getId().getTipoDocumento() + "-" + dh.getId().getSerieNumero() + "-B.xml";
+                buildFile(xmlBizlink, fileName);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void generateFile(DocumentHeader dh, FileWriter fichero, BufferedWriter bw) throws IOException {
+
+        StringBuilder s = new StringBuilder("");
+
+        if (dh.getBl_xml() != null) {
+            s.append(dh.getId().getNumeroDocumentoEmisor()).append("-").append(dh.getId().getTipoDocumento()).append("-").append(dh.getId().getSerieNumero()).append("-B.xml");
+        } else {
+            s.append(" ");
+        }
+
+        s.append("|");
+
+        if (dh.getBl_xmlubl() != null) {
+            s.append(dh.getId().getNumeroDocumentoEmisor()).append("-").append(dh.getId().getTipoDocumento()).append("-").append(dh.getId().getSerieNumero()).append(".xml");
+        } else {
+            s.append(" ");
+        }
+
+        s.append("|");
+
+        if (dh.getBl_cdr() != null) {
+            s.append("R").append(dh.getId().getNumeroDocumentoEmisor()).append("-").append(dh.getId().getTipoDocumento()).append("-").append(dh.getId().getSerieNumero()).append(".xml");
+        } else {
+            s.append(" ");
+        }
+
+        s.append("|");
+
+        if (dh.getBl_pdf() != null) {
+            s.append(dh.getBl_urlpdf());
+        } else {
+            s.append(" ");
+        }
+
+        s.append("|");
+
+        s.append(dh.getCodigoAuxiliar40_3()).append("|").append(dh.getFechaEmision());
+
+        //logger.log(Level.WARNING, "Validando linea de archivo............."+s.toString());
+        bw.write(s.append("\n").toString());
+    }
+
+    private void buildFile(byte[] rawData, String fileName) {
+        try {
+            File file = new File(fileName);
+            OutputStream out = new FileOutputStream(file);
+            out.write(rawData);
+            out.flush();
+            out.close();
+            sftp.put(new FileInputStream(file), file.getName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void disconnectToSftp() {
+        sftp.exit();
+        sftp.disconnect();
+        session.disconnect();
+    }
+
+    public List<Object> getListDocument() {
+        return listDocument;
+    }
+
+    /**
+     * @param listDocument the listDocument to set
+     */
+    public void setListDocument(List<Object> listDocument) {
+        this.listDocument = listDocument;
+    }
+
+    @Override
+    public void close() {
+        try {
+            this.disconnectToSftp();
+        } catch (Exception ex) {
+            logger.warning(() -> "Can't disconnect from sftp." + ex);
+        }
+    }
+}
