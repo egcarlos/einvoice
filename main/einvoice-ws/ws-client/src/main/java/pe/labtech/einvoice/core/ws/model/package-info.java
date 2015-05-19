afe766ea756c1396ab101c04ba19d0db13a7f694
Carlos Echeverría <egcarlos@gmail.com>
2015-05-18 19:05:56 -0500

    CLEANUP
    remove unused classes

--- main/einvoice-ws/ws-client/src/main/java/pe/labtech/einvoice/core/ws/model/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * El paquete contiene los elementos principales de modelo de datos para la
- * comunicación con la plataforma de facturación electrónica.
- */
-package pe.labtech.einvoice.core.ws.model;
